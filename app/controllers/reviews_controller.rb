6373b5b39745476dba9e0dd9d39604930576530d
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-06 13:24:26 -0700

    add review page, won't add a blank review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,17 @@
 class ReviewsController < ApplicationController
   def new
-    @product = Product.find(params[:recipe_id])
-    @reviews = @product.reviews.new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
     render :new
   end
 
   def create
-    @product = Product.find(params[:recipe_id])
+    @product = Product.find(params[:product_id])
     @review = @product.reviews.new(review_params)
     if @review.save
       redirect_to product_path(@product)
     else
-      redirect_to product_path(@product)
+      redirect_to new_product_review_path(@product)
     end
   end
 
@@ -31,7 +31,7 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
     if @review.update(review_params)
       # flash[:notice] = "Ingredient successfully updated!"
-      redirect_to product_path(@review.recipe)
+      redirect_to product_path(@review.product)
     else
       @product = Product.find(params[:product_id])
       render :edit
