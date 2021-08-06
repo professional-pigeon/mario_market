5f651000e87af012c1f2873a92ca517109deab50
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-06 15:10:00 -0700

    add all scoped products to front page in panels

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,9 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    @local = Product.local
+    @recent = Product.most_recent
+    @reviewed = Product.most_reviews
     render :index
   end
 
