217bdfdd3fba68535f50120ae106ee1a498ee3b3
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-06 15:17:07 -0700

    pass specs for titleizing a product on save

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,8 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_presence_of :origin }
   it { should validate_presence_of :cost }
+  it("titleizes the name of an product") do
+    product = Product.create({name: "hot dog", origin: "France", cost: 5.00})
+    expect(product.name).to(eq("Hot Dog"))
+  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,5 +6,4 @@ describe Review do
   it { should validate_presence_of :content_body }
   it { should validate_presence_of :rating }
   it { should validate_length_of(:content_body).is_at_most(250) }
-
 end
