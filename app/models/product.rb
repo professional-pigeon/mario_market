c92f459bb031f2ef01aeeea3c746809a703caa22
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-06 12:49:24 -0700

    add validations for product scope for local products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
+  validates :cost, presence: true
+  validates :origin, presence: true
+  scope :local, -> { where(origin: "USA") }
+
+
+  before_save(:titleize_product)
+
+  private
+    def titleize_product
+      self.name = self.name.titleize
+    end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 describe Product do
   it { should have_many(:reviews) }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :origin }
+  it { should validate_presence_of :cost }
 end
