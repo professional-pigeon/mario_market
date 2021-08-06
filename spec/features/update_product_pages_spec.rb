78a01606d45117ea1f9d7ba58cb9b5776ac2114c
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-06 15:43:08 -0700

    pass test for upating product in capybara

--- spec/features/update_product_pages_spec.rb
+++ spec/features/update_product_pages_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 describe "the update product process" do
   it "changes the product content" do
-    test_product = Product.new({name: => "Rice", origin: => "Arizona", cost: => 30})
-    visit products_path
-    click_link 'Rice'
+    Product.destroy_all
+    test_product = Product.new({:name => "Rice", :origin => "Arizona", :cost => 30})
+    test_product.save
+    visit product_path(test_product)
     click_on 'Edit Product Details'
     fill_in 'Name', :with => 'Zappo'
     fill_in 'Origin', :with => 'America'
     fill_in 'Cost', :with => 3.00
+    click_on 'Update Product'
     expect(page).to have_content 'Zappo'
   end
 
