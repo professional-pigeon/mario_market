fb99aaed84db1bf83dfb4d1d9b51d4f0678ff8ea
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-06 12:56:26 -0700

    add validation tests for reviews, pass tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, presence: true, uniqueness: {case_sensitive: false}
+  validates :rating, presence: true
+  validates :content_body, presence: true
 end
