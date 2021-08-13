33090092dfadc26870ba364115fc526dffef5b4e
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-13 16:33:05 -0700

    update rating validation to be in range of 1-5 and pass test for validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :product
   validates :author, presence: true
   validates :rating, presence: true
+  validates_inclusion_of :rating, in: 1..5
   validates :content_body, presence: true
   validates_length_of :content_body, maximum: 250
   validates_length_of :content_body, minimum: 50
